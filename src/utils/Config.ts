6a27efb434425a13d70aea7316a5bd3e1c3d5a4a
Eduardo Woll <eduwoll007@gmail.com>
2021-05-16 23:10:38 -0300

    Update Config.ts

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,6 +1,6 @@
 export const Config = {
   site_name: 'Classroom Admin',
-  title: 'Classroom Admin - Gerencie suas turmas e alunos do Google Classroom',
-  description: 'Gerencie suas turmas e alunos do Google Classroom de maneira mais prática',
+  title: 'Classroom Admin - Gerencie suas turmas do Google Classroom',
+  description: 'Com o Classroom Admin você pode visualizar todos os dados das turmas e alunos de sua instituição sem dificuldades.',
   locale: 'pt-br',
 };
